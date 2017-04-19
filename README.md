3e7f2c40ec3fe4b0acf3dd192fc95db9d0b1f6a3
kaa3n <knedaria@gmail.com>
2017-04-19 19:05:34 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,5 @@
+# lcd_web
+This is generator test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: lcd_web
+# Recipe:: default
+#
+# Copyright:: 2017, Kaab Nedaria, All Rights Reserved.
